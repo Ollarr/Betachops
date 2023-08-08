2fbfbc786b49a0f7ce16e8b0357b2f57aa766b43
Oluwasegun <oluyedis@gmail.com>
2023-08-08 14:26:58 +0100

    Feat: Added Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className="">
+      <div className="flex flex-col ">
+        <Navbar />
+
+        <main>{children}</main>
+      </div>
+      {/* <*** /> */}
+    </div>
+  );
+};
+
+export default Layout;
