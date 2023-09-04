9966d74ebeb2d0969fcc65e0b0fb2e28b9a14ec5
Oluwasegun <oluyedis@gmail.com>
2023-09-05 00:06:02 +0100

    Added contact and shop page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import Navbar from "../../components/Navbar";
+import Hero from "../../components/Hero";
+function LandingPage() {
+  return (
+    <div>
+      <Layout>
+        <p>Contact</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/pages/shop/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/Layout";
+function Shop() {
+  return (
+    <div className="w-full">
+      <Layout>
+        <div>
+          <p className="text-black text-center text-3xl">Shop Now</p>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+export default Shop;
