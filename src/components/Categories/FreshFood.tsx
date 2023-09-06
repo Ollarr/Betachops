05505b783fca7352677f046db459107dd63e0e12
Oluwasegun <oluyedis@gmail.com>
2023-09-06 18:25:00 +0100

    Added FreshFood category component

---
+++ src/components/Categories/FreshFood.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+function FreshFood() {
+	const freshFoodData = [
+		{
+			id: 1,
+			name: "Fresh Tomatoes",
+			price: 3500,
+			image:
+				"https://www.supermart.ng/cdn/shop/files/1504602987194_rspwxyz64_tomatoes_-_big_basket_180x@2x.jpg?v=1689688689",
+		},
+		{
+			id: 2,
+			name: "Fresh chicken",
+			price: 1500,
+
+			image:
+				"https://www.supermart.ng/cdn/shop/files/hubmt47_180x@2x.jpg?v=1692889611",
+		},
+		{
+			id: 3,
+			name: "Fresh carrot",
+			price: 3500,
+
+			image:
+				"https://www.supermart.ng/cdn/shop/files/spmt1066_180x@2x.jpg?v=1689133914",
+		},
+		{
+			id: 4,
+			name: "Fresh Pumpkin",
+			price: 1200,
+
+			image:
+				"https://www.supermart.ng/cdn/shop/files/omwpxyz1006_180x@2x.jpg?v=1689075149",
+		},
+		{
+			id: 5,
+			name: "Fresh potatoes",
+			price: 13500,
+
+			image:
+				"https://www.supermart.ng/cdn/shop/files/sprnw1131_180x@2x.jpg?v=1689134180",
+		},
+		{
+			id: 6,
+			name: "Fresh bell pepper",
+			price: 3500,
+
+			image:
+				"https://www.supermart.ng/cdn/shop/files/oyingbo020_180x@2x.jpg?v=1689904280",
+		},
+	];
+
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 4,
+			partialVisibilityGutter: 20,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2,
+			partialVisibilityGutter: 20,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+			partialVisibilityGutter: 20,
+		},
+	};
+
+	return (
+		<div className="px-12 mb-4">
+			<h2 className="text-2xl mb-4">Fresh Food </h2>
+			<Carousel responsive={responsive} partialVisbile>
+				{freshFoodData.map((item) => (
+					<div key={item.id} className="">
+						<div className="card w-60  ">
+							<div className="flex flex-col border py-4 px-2 items-center justify-center">
+								<img src={item.image} alt={item.name} className="w-full h-56" />
+								<h3>{item.name}</h3>
+								<h3>{item.price}</h3>
+							</div>
+						</div>
+					</div>
+				))}
+			</Carousel>
+		</div>
+	);
+}
+
+export default FreshFood;
