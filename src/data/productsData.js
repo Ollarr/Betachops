8f645a18f3a9098843b0566e2372a34a7ef5944e
Oluwasegun <oluyedis@gmail.com>
2023-09-11 06:51:40 +0100

    Added products data to mock products db

---
+++ src/data/productsData.js
@@ -0,0 +1,268 @@
+export const productsData = [
+	{
+		id: 1,
+		category: "drinks",
+		name: "Fanta",
+		price: 3500,
+		image:
+			"https://www.supermart.ng/cdn/shop/files/sprnw1286_180x@2x.jpg?v=1688838110",
+	},
+	{
+		id: 2,
+		category: "drinks",
+		name: "CWAY refill water",
+		price: 1500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/rspwxyz422_300x_2x_0205fd73-49fb-4957-945a-9dbb098546a1_300x@2x.webp?v=1690142380",
+	},
+	{
+		id: 3,
+		category: "drinks",
+		name: "Hollandia",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/spbvyo2102_220x@2x.png?v=1689066986",
+	},
+	{
+		id: 4,
+		category: "drinks",
+		name: "Bottled water",
+		price: 1200,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/1508946327351_okea193_eva_table_water_75_cl_pack_of_12_180x@2x.jpg?v=1689140320",
+	},
+	{
+		id: 5,
+		category: "drinks",
+		name: "5 Alive pulpy orange",
+		price: 13500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/products/5-Alive-Pulpy-Orange-85-cl-x6-Supermart-ng-1647_300x@2x.jpg?v=1690439608",
+	},
+	{
+		id: 6,
+		category: "drinks",
+		name: "Schweppes ginger",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/aspar061_300x@2x.jpg?v=1689903945",
+	},
+	{
+		id: 1,
+		category: "freshFood",
+		name: "Fresh Tomatoes",
+		price: 3500,
+		image:
+			"https://www.supermart.ng/cdn/shop/files/1504602987194_rspwxyz64_tomatoes_-_big_basket_180x@2x.jpg?v=1689688689",
+	},
+	{
+		id: 2,
+		category: "freshFood",
+		name: "Fresh chicken",
+		price: 1500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/hubmt47_180x@2x.jpg?v=1692889611",
+	},
+	{
+		id: 3,
+		category: "freshFood",
+		name: "Fresh carrot",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/spmt1066_180x@2x.jpg?v=1689133914",
+	},
+	{
+		id: 4,
+		category: "freshFood",
+		name: "Fresh Pumpkin",
+		price: 1200,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/omwpxyz1006_180x@2x.jpg?v=1689075149",
+	},
+	{
+		id: 5,
+		category: "freshFood",
+		name: "Fresh potatoes",
+		price: 13500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/sprnw1131_180x@2x.jpg?v=1689134180",
+	},
+	{
+		id: 6,
+		category: "freshFood",
+		name: "Fresh bell pepper",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/oyingbo020_180x@2x.jpg?v=1689904280",
+	},
+	{
+		id: 1,
+		category: "foodCupboard",
+		name: "Semovita",
+		price: 600,
+		image:
+			"https://www.supermart.ng/cdn/shop/files/1480692828943_spmrt471_golden_penny_semovita_500_g_180x@2x.jpg?v=1689119826",
+	},
+	{
+		id: 2,
+		category: "foodCupboard",
+		name: "Golden Penny Macaroni",
+		price: 550,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/spcgps881_300x_2x_53bce12c-1de9-4eb9-85c3-f76bf68eb751_180x@2x.webp?v=1690142441",
+	},
+	{
+		id: 3,
+		category: "foodCupboard",
+		name: "Knorr seasoning powder",
+		price: 950,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/narabc1435_220x@2x.png?v=1689105331",
+	},
+	{
+		id: 4,
+		category: "foodCupboard",
+		name: "Nutzy",
+		price: 800,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/spxtyabc643_300x_2x_6f8f5de8-ddd5-4008-87b0-07b5dc193067_300x@2x.webp?v=1690142642",
+	},
+	{
+		id: 5,
+		category: "foodCupboard",
+		name: "Green giant sweetcorn",
+		price: 13500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/sprnw1338_300x_2x_0078d88c-2d43-43ee-a065-01c7b3916bf0_180x@2x.webp?v=1690142503",
+	},
+	{
+		id: 6,
+		category: "foodCupboard",
+		name: "Titus Sardine",
+		price: 800,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/sprnw1336_300x_2x_39f9e909-3764-42a4-83c1-41de4c828977_300x@2x.webp?v=1690142464",
+	},
+	{
+		id: 1,
+		name: "Irish spring",
+		price: 3500,
+		category: "toiletries",
+		image:
+			"https://www.supermart.ng/cdn/shop/files/spar0874_180x@2x.jpg?v=1688683558",
+	},
+	{
+		id: 2,
+		category: "toiletries",
+		name: "Veet hair removal",
+		price: 1500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/OkeA3333_300x@2x.jpg?v=1689141720",
+	},
+	{
+		id: 3,
+		category: "toiletries",
+		name: "Bic comfort razor",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/mega1385_bic_comfort_2_razor_5_pieces_180x@2x.jpg?v=1689141885",
+	},
+	{
+		id: 4,
+		category: "toiletries",
+		name: "Johnson's cotton buds",
+		price: 1200,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/CITD2439_180x@2x.jpg?v=1688757019",
+	},
+	{
+		id: 5,
+		category: "toiletries",
+		name: "Euthymol toothpaste",
+		price: 13500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/11_1_180x@2x.jpg?v=1690560758",
+	},
+	{
+		id: 6,
+		category: "toiletries",
+		name: "Listerine Mouthwash",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/sptomw1063_listerine-mouthwash-coolmint_300x_2x_6829480a-42cf-4665-a951-7d6d5a2c9957_300x@2x.jpg?v=1690142602",
+	},
+	{
+		id: 1,
+		category: "electronics",
+		name: "Midea washer dryer front loader",
+		price: 3500,
+		image:
+			"https://www.supermart.ng/cdn/shop/files/dbk2136_180x@2x.jpg?v=1689139699",
+	},
+	{
+		id: 2,
+		category: "electronics",
+		name: "Scanfrost cooker",
+		price: 1500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/dbk1646_300x@2x.jpg?v=1688752806",
+	},
+	{
+		id: 3,
+		category: "electronics",
+		name: "LG Smart TV",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/dbk2012_300x@2x.jpg?v=1689133124",
+	},
+	{
+		id: 4,
+		category: "electronics",
+		name: "Rite-Tek blender",
+		price: 1200,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/SPARNW640_220x@2x.jpg?v=1688718672",
+	},
+	{
+		id: 5,
+		category: "electronics",
+		name: "CWAY water dispenser",
+		price: 13500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/dbk3958_300x@2x.jpg?v=1689140680",
+	},
+	{
+		id: 6,
+		category: "electronics",
+		name: "PS5 Console",
+		price: 3500,
+
+		image:
+			"https://www.supermart.ng/cdn/shop/files/dbk2079_300x@2x.jpg?v=1689134634",
+	},
+	// Add more products for different categories
+];
